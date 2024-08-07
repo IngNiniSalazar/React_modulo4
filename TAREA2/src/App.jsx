c18d899d7b5237300f9cef5691348c95d27a94c7
IngNiniSalazar <johannaquitian140@gmail.com>
2024-08-06 20:45:33 -0500

    se soluciona tarea 2

---
+++ TAREA2/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <h1>SHAKIRA</h1>
+      
+      <img className='img' src="/RS1389-90_Shakira_Look-4_0032_V4.webp" alt='imagen' />
+      <p class="centrado">
+      Shakira Isabel Mebarak Ripoll (Barranquilla, Colombia, 1977), 
+      de nombre artístico Shakira, es una cantante, compositora, 
+      bailarina y productora. Gracias a su potencia y versatilidad 
+      vocal, y a unas ventas millonarias que superan los 75 millones 
+      en todo el mundo, se la considera como la Reina del pop latino.
+      </p>
+    </div>
+  );
+}
+
+export default App
